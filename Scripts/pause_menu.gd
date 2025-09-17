6abfa99c5a766f5e3a97eafb9d3debccfb61c261
Gabriel Panza <gabrieljpanza@gmail.com>
2025-09-16 21:52:53 -0300

    Implementation part 2/2
    PauseMenu implemented

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,37 +1,24 @@
 extends Control
 
-@onready var video_player: VideoStreamPlayer = $PauseVideo
-@onready var menu_container: Control = $"."
-@onready var resume_button: Button = $VBoxContainer/ResumeButton
-@onready var quit_button: Button = $VBoxContainer/QuitButton
+@onready var video_player: VideoStreamPlayer = $CanvasLayer/PauseVideo
+@onready var menu_container: CanvasLayer = $CanvasLayer
+@onready var resume_button: Button = $CanvasLayer/VBoxContainer/ResumeButton
+@onready var quit_button: Button = $CanvasLayer/VBoxContainer/QuitButton
 
 
 func _ready() -> void:
-	hide()
-	video_player.finished.connect(_on_video_finished)
-	resume_button.pressed.connect(_on_resume_pressed)
-	quit_button.pressed.connect(_on_quit_pressed)
-
-# Esta é a função que será chamada de fora para iniciar o processo de pausa.
-func start_pause() -> void:
-	get_tree().paused = true  # Isso aqui pode bugar a interação com o botão
-	show()
 	menu_container.hide()
-	video_player.play()
-
-# Chamada automaticamente quando o sinal "finished" do VideoStreamPlayer é emitido.
-func _on_video_finished() -> void:
-	video_player.paused = true  # Isso aqui pode bugar a interação com o botão
+ 	
+func start_pause() -> void:
 	menu_container.show()
+	video_player.play()
+	video_player.paused = false
 
+func _on_resume_button_pressed() -> void:
+	menu_container.hide()
 
-# Chamada quando o botão "Continuar" é pressionado.
-func _on_resume_pressed() -> void:
-	get_tree().paused = false   # Isso aqui pode bugar a interação com o botão
-	queue_free()
-
-
-# Chamada quando o botão "Sair" é pre*ssionado.*
-func _on_quit_pressed() -> void:
-	get_tree().paused = false  # Isso aqui pode bugar a interação com o botão
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_pause_video_finished() -> void:
+	video_player.paused = true
