154306dd0017e1a11e3c1feb6f5c0c32e80b808f
ooJungle <sr.jgribeiro10@gmail.com>
2025-09-20 13:06:22 -0300

    resolvendo bogs

--- Scripts/Menu/main_menu.gd
+++ Scripts/Menu/main_menu.gd
@@ -4,7 +4,7 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Cenas/Fases/fase_teste.tscn")
 
 func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas/SettingsMenu.tscn")
+	get_tree().change_scene_to_file("res://Cenas/Menu/SettingsMenu.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
--- Scripts/Menu/settings_menu.gd
+++ Scripts/Menu/settings_menu.gd
@@ -7,4 +7,4 @@ func _on_h_slider_value_changed(value: float) -> void:
 	Global.volume = value
 	
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://Cenas/Menu/MainMenu.tscn")
