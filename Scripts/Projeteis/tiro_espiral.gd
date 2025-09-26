f038a09e4d70bde2faae05d32c217099a549b8d2
gustavinholegal <guspessurno@gmail.com>
2025-09-26 19:24:11 -0300

    Update tiro_espiral.gd

--- Scripts/Projeteis/tiro_espiral.gd
+++ Scripts/Projeteis/tiro_espiral.gd
@@ -15,4 +15,4 @@ func _on_body_entered(body: Node2D) -> void:
 	queue_free()
 
 func _physics_process(delta: float) -> void:
-	position = position.rotated(90*delta)
+	position = position.rotated(9*delta)
