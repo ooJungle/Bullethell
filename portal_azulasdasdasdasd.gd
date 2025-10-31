1f7917537ca4c0704a01ad66a2c90f2f64850b36
Gabriel Panza <gabrieljpanza@gmail.com>
2025-10-30 21:10:03 -0300

    Update fase_0 portais

--- portal_azulasdasdasdasd.gd
+++ portal_azulasdasdasdasd.gd
@@ -2,11 +2,9 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	body.position = Vector2(1716.0, 10.0)
-	$"../inimigo_4".visible = true
-	$"../inimigo_5".visible = true
-	$"../inimigo_9".visible = true
-	$"../inimigo_10".visible = true
-	$"../inimigo_11".visible = true
-	$"../inimigo_12".visible = true
-	$"../inimigo_13".visible = true
-	$"../inimigo_15".visible = true
+	$"../inimigo_1".visible = true
+	$"../inimigo_polvo".visible = true
+	$"../inimigo_polvo2".visible = true
+	$"../inimigo_3".visible = true
+	$"../inimigo_polvo4".visible = true
+	$"../inimigo_polvo5".visible = true
