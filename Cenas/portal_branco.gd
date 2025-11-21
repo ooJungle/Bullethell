0534edd472b31b76549835b6a408c4f08c11ddf6
gustavinholegal <guspessurno@gmail.com>
2025-11-21 15:48:46 -0300

    chroma

--- Cenas/portal_branco.gd
+++ Cenas/portal_branco.gd
@@ -1,4 +1,7 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	body.position = Vector2(112.0, 1065.0)
+	if Global.portais_ativos["Fase_espaco"] == false and Global.portais_ativos["Fase_plat"] == false and Global.portais_ativos["Fase_RPG"] == false:
+		body.position = Vector2(-300.0, 2160.0)
+	else:
+		body.position = Vector2(112.0, 1065.0)
