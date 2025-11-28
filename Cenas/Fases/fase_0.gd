e5810bfab5819062568dd360cad88e286a6200f8
gustavinholegal <guspessurno@gmail.com>
2025-11-28 20:27:11 -0300

    cara má

--- Cenas/Fases/fase_0.gd
+++ Cenas/Fases/fase_0.gd
@@ -7,12 +7,17 @@ extends Node2D
 ]
 
 @onready var color_rect: ColorRect = $Ambiente/ColorRect
+@onready var imagem_final: TextureRect = $Ambiente/ImagemFinal
 
 func _ready() -> void:
 	get_tree().get_root().set_transparent_background(true)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 0)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true, 0)
 	
+	if imagem_final:
+		imagem_final.visible = false
+		imagem_final.modulate.a = 0.0
+
 	$Timer.start()
 	Global.plataforma = false
 	
@@ -27,18 +32,32 @@ func verificar_fim_de_jogo():
 	var f2 = Global.portais_ativos["Fase_plat"] == false
 	var f3 = Global.portais_ativos["Fase_RPG"] == false
 	
-	if f1 and f2 and f3 and not Global.dialogo_final_mostrado:
-		iniciar_dialogo_automatico()
+	if f1 and f2 and f3:
+		mostrar_imagem_final()
+		if not Global.dialogo_final_mostrado:
+			iniciar_dialogo_automatico()
 
+func mostrar_imagem_final():
+	if not imagem_final: return
+	
+	imagem_final.visible = true
+	imagem_final.modulate.a = 0.0
+	
+	var tween = create_tween()
+	
+	tween.tween_property(imagem_final, "modulate:a", 0.5, 2.0)
+	tween.tween_interval(4.0)
+	tween.tween_property(imagem_final, "modulate:a", 0.0, 2.0)
+	
+	tween.tween_callback(func(): 
+		imagem_final.visible = false
+	)
+	
 func iniciar_dialogo_automatico():
 	Global.dialogo_final_mostrado = true
-	
-	# Chama o efeito visual
 	animar_pulso_final()
-	
 	Dialogo.start_dialogue(falas_finais)
 
-# --- FUNÇÃO DO EFEITO VISUAL ---
 func animar_pulso_final():
 	if not color_rect:
 		return
@@ -49,14 +68,9 @@ func animar_pulso_final():
 
 	var tween = create_tween()
 	
-	# Configuração dos valores da vinheta (ajuste se necessário)
 	var base_opacity = 0.5
-	var peak_opacity = 1.0 # Escurece totalmente as bordas
+	var peak_opacity = 1.0
 	
-	# Cria a sequência de 3 pulsos
 	for i in range(3):
-		# 1. Escurece (Aumenta opacidade da vinheta)
 		tween.tween_property(material, "shader_parameter/vignette_opacity", peak_opacity, 1.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-		
-		# 2. Clareia (Volta ao normal)
 		tween.tween_property(material, "shader_parameter/vignette_opacity", base_opacity, 1.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
