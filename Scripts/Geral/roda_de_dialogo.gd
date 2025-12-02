8b36ec0feddbc7ecfb191b23157faa2ce7a38665
ooJungle <117485499+ooJungle@users.noreply.github.com>
2025-12-02 14:52:41 -0300

    Mega organização nas pastas

---
+++ Scripts/Geral/roda_de_dialogo.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready():
+	if has_node("RodaVisual"):
+		$RodaVisual.abertura = 0.0
+		
+		var tween = create_tween()
+		tween.tween_property($RodaVisual, "abertura", 1.0, 1.9).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+
+func _on_player_morreu():
+	RodaManager.fechar_roda("perdeu")
+
+func _on_opcao_certa_selecionada():
+	RodaManager.fechar_roda("venceu")
