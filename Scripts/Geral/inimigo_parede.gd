180426dd9dac9db117fba519e9457af73f4b29af
ooJungle <117485499+ooJungle@users.noreply.github.com>
2025-12-02 15:58:36 -0300

    tiro inimigo parede

--- Scripts/Geral/inimigo_parede.gd
+++ Scripts/Geral/inimigo_parede.gd
@@ -1,57 +1,51 @@
 extends CharacterBody2D
 
-@export var speed: float = 50.0
-@onready var ray_frente: RayCast2D = $RayCast_Frente
-@onready var ray_parede: RayCast2D = $RayCast_Parede
+@export var speed = 60.0
+@export var forca_gravidade = 2000.0
+@export var velocidade_rotacao = 15.0
 @export var velocidade_tiro: float = 200.0
+
 var projetil_cena = preload("uid://bgs1gd3re016g")
-var girando: bool = false
-var em_cooldown: bool = false
 var player: Node2D
 var attack_cooldown: float = 0
+
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("players")
 	add_to_group("inimigo")
 
-func _physics_process(delta: float) -> void:
-	if Global.paused:
+func _physics_process(delta):
+	if "paused" in Global and Global.paused:
 		return
 
-	if girando or em_cooldown:
-		mover_com_imã()
-		return
-
-	if ray_frente.is_colliding():
-		iniciar_giro(-90)
-		return
-
-	if not ray_parede.is_colliding():
-		await mover_um_pouco_para_frente(0.1)
-		if not em_cooldown: 
-			iniciar_giro(90)
-		return
-	mover_com_imã()
+	velocity.x = speed
+	velocity.y += forca_gravidade * delta
+	
+	var velocidade_final = velocity.rotated(rotation)
+	velocity = velocidade_final
+	
+	move_and_slide()
+	
+	velocity = velocity.rotated(-rotation)
+	velocity.x = speed
+	
+	if (is_on_floor() or is_on_wall() or is_on_ceiling()) and get_slide_collision_count() > 0:
+		var normal = get_slide_collision(0).get_normal()
+		alinharNormal(normal, delta)
+	elif ray_cast_2d.is_colliding():
+		var normal = ray_cast_2d.get_collision_normal()
+		alinharNormal(normal, delta)
+	else:
+		rotate(deg_to_rad(velocidade_rotacao * 10) * delta)
+		
 	attack_cooldown += delta
-	if (player.global_position - global_position).length() < 500:
+	if player and (player.global_position - global_position).length() < 500:
 		shoot()
 
-func mover_com_imã():
-	velocity = (transform.x * speed) + (transform.y * 100)
-	move_and_slide()
-
-func iniciar_giro(graus: float):
-	girando = true
-	em_cooldown = true
-	rotation_degrees += graus
-	await get_tree().physics_frame 
-	girando = false
-	await get_tree().create_timer(0.5).timeout
-	em_cooldown = false
-
-func mover_um_pouco_para_frente(tempo: float):
-	girando = true
-	await get_tree().create_timer(tempo).timeout
+func alinharNormal(normal: Vector2, delta: float):
+	var target_rotation = normal.angle() + PI / 2
+	rotation = lerp_angle(rotation, target_rotation, velocidade_rotacao * delta)
 
 func shoot():
 	if attack_cooldown >= 3:
